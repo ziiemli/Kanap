a373696c13b817249135de2715d0d9429221713b
ziiemli <zilin.moulin@outlook.fr>
2022-10-02 16:47:39 +0200

    form in progress

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,64 +3,104 @@
 let registeredItem = JSON.parse(localStorage.getItem("item")) || [];
 console.log(registeredItem);
 
-
-let data = [];
-const recoverArticle = async function () {
-  await registeredItem.forEach(item => {
-    //inject ID of product
-    fetch(`http://localhost:3000/api/products/`)
-    //transform JSON format
-    .then(response => response.json())
-    //data
-    .then(data => {
-        data;
+//_____Recover products from API
+const recoverProducts = async function () {
+    await registeredItem.map(item => {
+        fetch(`http://localhost:3000/api/products/${item.idValue}`)
+            //transform JSON format
+            .then(response => response.json())
+            //data
+            .then(data => {
+                data.price;
+                item.name = data.name;
+                item.price = data.price;
+                item.imageUrl = data.imageUrl;
+                item.altTxt = data.altTxt;
+                item.description = data.description;
+            })
     })
-  });
-} 
-console.log(data);
-
-
-recoverArticle();
-
-// async function displayCart() {
-//   await registeredItem.forEach (article => {
-//     let selectItemID = article.idValue;
-//     let selectItemName = article.name; 
-//     let selectItemImage = article.imageUrl; 
-//   });
-// };
-// displayCart();
-
-
-  
+};
+recoverProducts();
+console.log(registeredItem);
     
 
 //_____Display items
-// const itemsDisplay = async function () {
-//     // await displayCart();
-//     document.getElementById("cart__items").innerHTML = registeredItem.map(item =>
-//         `<article class="cart__item" data-id="${item._idValue}" data-color="{product-color}">
-//         <div class="cart__item__img">
-//           <img src="${selectItemImage}" alt="${item.altTxt}">
-//         </div>
-//         <div class="cart__item__content">
-//           <div class="cart__item__content__description">
-//             <h2>Nom du produit</h2>
-//             <p>${item.colorValue}</p>
-//             <p>42,00 €</p>
-//           </div>
-//           <div class="cart__item__content__settings">
-//             <div class="cart__item__content__settings__quantity">
-//               <p>Qté : </p>
-//               <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-//             </div>
-//             <div class="cart__item__content__settings__delete">
-//               <p class="deleteItem">Supprimer</p>
-//             </div>
-//           </div>
-//         </div>
-//       </article>`
-//         )
-// };
-// itemsDisplay();
+const itemsDisplay = async function () {
+    // await displayCart();
+    await recoverProducts();
+    console.log(registeredItem[0].colorValue);
+    document.getElementById("cart__items").innerHTML = registeredItem.map(function(item) {
+        return console.log(item["name"]);
+    })
+};
+itemsDisplay();
 
+//_____FORM
+function form () {
+    //select input Tag
+    let formInput = document.getElementsByTagName("input");
+
+    //select each input
+    const firstName = formInput.firstName;
+    const lastName = formInput.lastName;
+    const address = formInput.address;
+    const city = formInput.city;
+    const email = formInput.email;
+    
+    //creation RegEx
+    let firstNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
+    let lastNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
+    let addressRegEx =/^([0-9\s]{0,4})+([A-Za-zÀ-Üà-ü-.\s]{2,60})$/g;
+    let cityRegEx =/^([A-Za-zÀ-Üà-ü-.'\s]{2,40})$/g;
+    let emailRegEx = new RegExp(/^([\w!#$%&'*+/=?^`{|}~-]{2}(?:[.]?[\w!#$%&'*+/=?^`{|}~-]){0,60})@([a-z]{2,246}[a-z.-]?)+([.][a-z]{2,3})$/g);
+    
+    //True/False Value First Name 
+    firstName.addEventListener("change", () => {
+        if (firstNameRegEx.test(firstName.value)) {
+        }
+        else {
+            document.getElementById("firstNameErrorMsg").innerHTML = `Veuillez renseigner un prénom valide`
+        }
+    })
+    //True/False Value Last Name 
+    lastName.addEventListener("change", () => {
+        if (lastNameRegEx.test(lastName.value)) {
+        }
+        else {
+            document.getElementById("lastNameErrorMsg").innerHTML = `Veuillez renseigner un nom valide`
+        }
+    })
+    //True/False Value Address Name
+    address.addEventListener("change", () => {
+        if (addressRegEx.test(address.value)) {
+        }
+        else {
+            document.getElementById("addressErrorMsg").innerHTML = `Veuillez renseigner une adresse valide`
+        }
+    })
+    //True/False Value City Name
+    city.addEventListener("change", () => {
+        if (cityRegEx.test(city.value)) {
+        }
+        else {
+            document.getElementById("cityErrorMsg").innerHTML = `Veuillez renseigner une ville valide`
+        }
+    })
+    //True/False Value Email Name
+    email.addEventListener("change", () => {
+        if (emailRegEx.test(email.value)) {
+        }
+        else {
+            document.getElementById("emailErrorMsg").innerHTML = `Veuillez renseigner une adresse email valide`
+        }
+    })
+
+}
+form();
+
+
+
+
+
+    
+    
