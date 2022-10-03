adb824f87f228d57a48b5d932bf7eb777af72d94
ziiemli <zilin.moulin@outlook.fr>
2022-10-03 19:58:52 +0200

    display cart items, total quantity, total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,26 +3,37 @@
 let registeredItem = JSON.parse(localStorage.getItem("item")) || [];
 console.log(registeredItem);
 
-//_____Recover products from API
+//_____Cart init
+const cartInit = () => {
+    //loop through localStorage to recover each informations of item
+    Promise.all(registeredItem.map(async item => {
+        const r = await recoverProducts(item);
+        return r;
+    })).then((data) => {
+        //call functions with data as parameters
+        itemsDisplay(data);
+        totalDisplay(data);
+       console.log(data);
+    })
+}
+cartInit();
+
+//_____Recover item informations from API and add infos of localStorage
 async function recoverProducts(item) {
-    await fetch(`http://localhost:3000/api/products/${item.idValue}`)
-        //transform JSON format
-        .then(response => response.json())
-        //data
-        .then(data => {
-            data;
-            item.name = data.name;
-            item.price = data.price;
-            // console.log(item.price);
-            item.imageUrl = data.imageUrl;
-            item.altTxt = data.altTxt;
-            item.description = data.description;
-        })
+    //call data API
+    const result = await fetch(`http://localhost:3000/api/products/${item.idValue}`);
+    //transform data result in JSON
+    const obj = await result.json();
+    //add quantityValue and colorValue from localStorage to obj
+    obj.quantity = item.quantityValue;
+    obj.color = item.colorValue;
+    return obj;
 };
 
-const itemsDisplay = () => {
-    registeredItem.map(async item => {
-        await recoverProducts(item);
+//_____Items Display
+const itemsDisplay = (data) => {
+    //loop through data to display each product
+    data.forEach( item => {
         document.getElementById("cart__items").innerHTML += 
         `<article class="cart__item" data-id="${item.idValue}" data-color="${item.colorValue}">
             <div class="cart__item__img">
@@ -31,13 +42,13 @@ const itemsDisplay = () => {
             <div class="cart__item__content">
             <div class="cart__item__content__description">
                 <h2>${item.name}</h2>
-                <p>${item.colorValue}</p>
+                <p>${item.color}</p>
                 <p>${item.price}</p>
             </div>
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                 <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantityValue}">
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                 <p class="deleteItem">Supprimer</p>
@@ -47,51 +58,42 @@ const itemsDisplay = () => {
       </article>`
     })
 }
-itemsDisplay();
 
-
-var totalPrice = [];
-var totalQuantity = [];
-//_____Display Total Price and Quantity
-const displayTotalPrice = () => {
-        registeredItem.map(async item => {
-        await recoverProducts(item);
-        price = parseInt(item.price)*parseInt(item.quantityValue);
-        console.log(price);
-        totalPrice.push(price);
+//display sumPrices and quantity 
+function totalDisplay (data) {
+    //init totalQuantity and totalPrice
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    //loop through data to calculate totalQuantity and totalPrice
+    data.forEach(d => {
+        totalQuantity += d.quantity
+        totalPrice += d.price * d.quantity
     })
-    
-    console.log(totalPrice);
-} 
-displayTotalPrice();
-
-
-// //calculate Total of Price
-const displayPrice = totalPrice.reduce((previousValue, currentValue) => {
-    return (previousValue, + currentValue);
-});
-console.log(displayPrice);
+    //display totalQuantity and totalPrice
+    document.getElementById("totalPrice").innerText = totalPrice
+    document.getElementById("totalQuantity").innerText = totalQuantity
+}
 
 
 //_____FORM
-function form () {
-    //select input Tag
-    let formInput = document.getElementsByTagName("input");
+//select input Tag
+let formInput = document.getElementsByTagName("input");
 
-    //select each input
-    const firstName = formInput.firstName;
-    const lastName = formInput.lastName;
-    const address = formInput.address;
-    const city = formInput.city;
-    const email = formInput.email;
-    
-    //creation RegEx
-    let firstNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
-    let lastNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
-    let addressRegEx =/^([0-9\s]{0,4})+([A-Za-zÀ-Üà-ü-.\s]{2,60})$/g;
-    let cityRegEx =/^([A-Za-zÀ-Üà-ü-.'\s]{2,40})$/g;
-    let emailRegEx = /^([\w!#$%&'*+/=?^`{|}~-]{2}(?:[.]?[\w!#$%&'*+/=?^`{|}~-]){0,60})@([a-z]{2,246}[a-z.-]?)+([.][a-z]{2,3})$/g;
-    
+//select each input
+const firstName = formInput.firstName;
+const lastName = formInput.lastName;
+const address = formInput.address;
+const city = formInput.city;
+const email = formInput.email;
+
+//creation RegEx
+let firstNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
+let lastNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
+let addressRegEx =/^([0-9\s]{0,4})+([A-Za-zÀ-Üà-ü-.\s]{2,60})$/g;
+let cityRegEx =/^([A-Za-zÀ-Üà-ü-.'\s]{2,40})$/g;
+let emailRegEx = /^([\w!#$%&'*+/=?^`{|}~-]{2}(?:[.]?[\w!#$%&'*+/=?^`{|}~-]){0,60})@([a-z]{2,246}[a-z.-]?)+([.][a-z]{2,3})$/g;
+//Verify formValues
+function form () { 
     //True/False Value First Name 
     firstName.addEventListener("change", () => {
         if (firstNameRegEx.test(firstName.value)) {
@@ -136,7 +138,29 @@ function form () {
 }
 form();
 
+const formValues = {
+    firstNameValue: firstName.value,
+    lastNameValue: lastName.value,
+    addressValue: address.value,
+    cityValue: city.value,
+    emailValue: email.value
+}
 
+console.log(formValues);
+
+const sendOrder = {
+    formValues, registeredItem
+};
+
+// const p = () => {
+//     fetch (`http://localhost:3000/api/products/order`, {
+//         method: "POST"
+//         body: JSON.stringify(sendOrder),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+// }
 
 
 
