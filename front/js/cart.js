4e2d61a8cb8222f9e71c4d9951b4a1350f342bc0
ziiemli <zilin.moulin@outlook.fr>
2022-10-04 18:57:07 +0200

    delete item

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,257 +1,289 @@
 //recover localStorage
 //reading localStorage and transform JSON in JS or create new table if it doesn't exist
-let registeredItem = JSON.parse(localStorage.getItem("item")) || [];
-console.log(registeredItem);
+let registeredItem = JSON.parse(localStorage.getItem("item")) || []
+console.log(registeredItem)
 
 //_____Cart init
 const cartInit = () => {
     //loop through localStorage to recover each informations of item
-    Promise.all(registeredItem.map(async item => {
-        const r = await recoverProducts(item);
-        return r;
-    })).then((data) => {
+    Promise.all(
+        registeredItem.map(async (item) => {
+            const r = await recoverProducts(item)
+            return r
+        })
+    ).then((data) => {
         //call functions with data as parameters
-        itemsDisplay(data);
-        totalDisplay(data);
-        console.log(data);
+        itemsDisplay(data)
+        totalDisplay(data)
+        console.log(data)
     })
 }
-cartInit();
+cartInit()
 
 //_____Recover item informations from API and add infos of localStorage
 async function recoverProducts(item) {
     //call data API
-    const result = await fetch(`http://localhost:3000/api/products/${item.idValue}`);
+    const result = await fetch(`http://localhost:3000/api/products/${item.idValue}`)
     //transform data result in JSON
-    const obj = await result.json();
+    const obj = await result.json()
     //add quantityValue and colorValue from localStorage to obj
-    obj.quantity = item.quantityValue;
-    obj.color = item.colorValue;
-    return obj;
-};
+    obj.quantity = item.quantityValue
+    obj.color = item.colorValue
+    return obj
+}
 
 //_____Items Display
 const itemsDisplay = (data) => {
     //loop through data to display each product
-    data.forEach( item => {
-        const displayCard = document.getElementById("cart__items");
+    data.forEach((item) => {
+        const displayCard = document.getElementById("cart__items")
         //create HTML Product Card
         //_____Article
-        const article = document.createElement("article");
-        article.classList = "cart__item";
-        article.dataset.id = item._id;
-        article.dataset.color = item.color;
+        const article = document.createElement("article")
+        article.classList = "cart__item"
+        article.dataset.id = item._id
+        article.dataset.color = item.color
 
         //____Image
-        const imageContainer = document.createElement("div");
-        imageContainer.classList = "cart__item__img";
-        article.appendChild(imageContainer);
+        const imageContainer = document.createElement("div")
+        imageContainer.classList = "cart__item__img"
+        article.appendChild(imageContainer)
 
         const image = document.createElement("img")
-        image.src = item.imageUrl;
-        image.alt = item.altTxt;
-        imageContainer.appendChild(image);
+        image.src = item.imageUrl
+        image.alt = item.altTxt
+        imageContainer.appendChild(image)
 
         //___informations Container
-        const informationsContainer = document.createElement("div");
-        informationsContainer.classList = "cart__item__content";
-        article.appendChild(informationsContainer);
-        
+        const informationsContainer = document.createElement("div")
+        informationsContainer.classList = "cart__item__content"
+        article.appendChild(informationsContainer)
+
         //__informations description content
-        const descriptionContent = document.createElement("div");
-        descriptionContent.classList = "cart__item__content__description";
-        informationsContainer.appendChild(descriptionContent);
+        const descriptionContent = document.createElement("div")
+        descriptionContent.classList = "cart__item__content__description"
+        informationsContainer.appendChild(descriptionContent)
 
         //h2
-        const descriptionName = document.createElement("h2");
-        const descriptionNameText = document.createTextNode(`${item.name}`);
-        descriptionName.appendChild(descriptionNameText);
-        descriptionContent.appendChild(descriptionName);
+        const descriptionName = document.createElement("h2")
+        const descriptionNameText = document.createTextNode(`${item.name}`)
+        descriptionName.appendChild(descriptionNameText)
+        descriptionContent.appendChild(descriptionName)
         //color
-        const descriptionColor = document.createElement("p");
-        const descriptionColorText = document.createTextNode(`${item.color}`);
-        descriptionColor.appendChild(descriptionColorText);
-        descriptionContent.appendChild(descriptionColor);
+        const descriptionColor = document.createElement("p")
+        const descriptionColorText = document.createTextNode(`${item.color}`)
+        descriptionColor.appendChild(descriptionColorText)
+        descriptionContent.appendChild(descriptionColor)
         //price
-        const descriptionPrice = document.createElement("p");
-        const descriptionPriceText = document.createTextNode(`${item.price} €`);
-        descriptionPrice.appendChild(descriptionPriceText);
-        descriptionContent.appendChild(descriptionPrice);
+        const descriptionPrice = document.createElement("p")
+        const descriptionPriceText = document.createTextNode(`${item.price} €`)
+        descriptionPrice.appendChild(descriptionPriceText)
+        descriptionContent.appendChild(descriptionPrice)
 
         //__informations settings content
-        const settingsContent = document.createElement("div");
-        settingsContent.classList = "cart__item__content__settings";
-        informationsContainer.appendChild(settingsContent);
+        const settingsContent = document.createElement("div")
+        settingsContent.classList = "cart__item__content__settings"
+        informationsContainer.appendChild(settingsContent)
 
         //__quantity
-        const settingsQuantity = document.createElement("div");
-        settingsQuantity.classList = "cart__item__content__settings__quantity";
-        settingsContent.appendChild(settingsQuantity);
+        const settingsQuantity = document.createElement("div")
+        settingsQuantity.classList = "cart__item__content__settings__quantity"
+        settingsContent.appendChild(settingsQuantity)
 
-        const quantity = document.createElement("p");
-        const quantityText = document.createTextNode(`Qté : `);
-        quantity.appendChild(quantityText);
-        settingsQuantity.appendChild(quantity);
-        
+        const quantity = document.createElement("p")
+        const quantityText = document.createTextNode(`Qté : `)
+        quantity.appendChild(quantityText)
+        settingsQuantity.appendChild(quantity)
+
         const inputQuantity = document.createElement("input")
-        inputQuantity.type = "number";
-        inputQuantity.classList = "itemQuantity";
-        inputQuantity.name = "itemQuantity";
-        inputQuantity.min = 1;
-        inputQuantity.max = 100;
-        inputQuantity.value = item.quantity;
-        settingsQuantity.appendChild(inputQuantity);
-        
+        inputQuantity.type = "number"
+        inputQuantity.classList = "itemQuantity"
+        inputQuantity.name = "itemQuantity"
+        inputQuantity.min = 1
+        inputQuantity.max = 100
+        inputQuantity.value = item.quantity
+        settingsQuantity.appendChild(inputQuantity)
+
         //delete
         const settingsDelete = document.createElement("div")
-        settingsDelete.classList = "cart__item__content__settings__delete";
-        settingsContent.appendChild(settingsDelete);
-        
-        const Delete = document.createElement("p");
+        settingsDelete.classList = "cart__item__content__settings__delete"
+        settingsContent.appendChild(settingsDelete)
+
+        const Delete = document.createElement("p")
         Delete.classList = "cart__item__content__settings__delete"
-        const DeleteText = document.createTextNode("Supprimer");
-        DeleteText.classList = "deleteItem";
-        Delete.appendChild(DeleteText);
-        settingsDelete.appendChild(Delete);
+        const DeleteText = document.createTextNode("Supprimer")
+        DeleteText.classList = "deleteItem"
+        Delete.appendChild(DeleteText)
+        settingsDelete.appendChild(Delete)
 
         //Article to display Card
-        displayCard.appendChild(article);
-
+        displayCard.appendChild(article)
 
         //_____CHANGE QUANTITY_____//
-        inputQuantity.addEventListener("change", event => {
-            changeQuantity(event.target, item);
+        inputQuantity.addEventListener("change", (event) => {
+            changeQuantity(event.target, item)
         })
-        
+        //_____Delete Product_____//
+        Delete.addEventListener("click", (event) => {
+            deleteProduct(event.target, item)
+        })
     })
 }
 
-//_____display sumPrices and quantity 
-function totalDisplay (data) {
+//_____display sumPrices and quantity
+function totalDisplay(data) {
     //init totalQuantity and totalPrice
-    let totalQuantity = 0;
-    let totalPrice = 0;
+    let totalQuantity = 0
+    let totalPrice = 0
     //loop through data to calculate totalQuantity and totalPrice
-    data.forEach(d => {
-        totalQuantity += parseInt(d.quantity);
-        totalPrice += parseInt(d.price) * parseInt(d.quantity);
+    data.forEach((d) => {
+        totalQuantity += parseInt(d.quantity)
+        totalPrice += parseInt(d.price) * parseInt(d.quantity)
     })
     //display totalQuantity and totalPrice
     document.getElementById("totalPrice").innerText = totalPrice
     document.getElementById("totalQuantity").innerText = totalQuantity
 }
 
-//____Change quantity
-function changeQuantity (event, item) {
-    
+//compare the selected product and the localStorage product
+function compareProduct (item) {
+    return registeredItem.find((p) => 
+    item._id === p.idValue && 
+    item.color === p.colorValue)
+}
 
-    let sameProduct = registeredItem.find(p => 
-        item._id === p.idValue && 
-        item.color === p.colorValue
-    );
+
+//____Change quantity
+function changeQuantity(event, item) {
+    let sameProduct = compareProduct(item);
     //if same item in localStorage > modify quantity
     if (sameProduct) {
-        //recover totalPrice
+        //recover totalPrice & quantity
         let totalPriceContainer = document.getElementById("totalPrice")
         let totalPrice = parseInt(totalPriceContainer.innerText)
+        let totalQuantityContainer = document.getElementById("totalQuantity")
+        let totalQuantity = parseInt(totalQuantityContainer.innerText)
 
+        //change price >  subtract value, add new value
+        totalPrice -= item.price * item.quantity
+        totalPrice += item.price * event.value
         //change quantity
-        totalPrice -= item.price * item.quantity;
-        totalPrice += item.price * event.value;
+        totalQuantity -= parseInt(item.quantity)
+        totalQuantity += parseInt(event.value)
+        // totalQuantity = totalQuantity--;
+        console.log(totalQuantity)
 
         //refresh display totalPrice
-        let newTotalPriceText = document.createTextNode(totalPrice);
-        totalPriceContainer.appendChild(newTotalPriceText);
-        totalPriceContainer.removeChild(totalPriceContainer.firstChild);
+        let newTotalPriceText = document.createTextNode(totalPrice)
+        totalPriceContainer.appendChild(newTotalPriceText)
+        totalPriceContainer.removeChild(totalPriceContainer.firstChild)
+        //refresh display quantity
+        let newTotalQuantityText = document.createTextNode(totalQuantity)
+        totalQuantityContainer.appendChild(newTotalQuantityText)
+        totalQuantityContainer.removeChild(totalQuantityContainer.firstChild)
 
-        //add localStorage item quantity and quantity selected, parseInt > transform string in entire number
-        item.quantity = event.value;
-        console.log(item.quantity);
-        console.log(event.value);
+        //assignto item.quantity the new quantity value
+        item.quantity = event.value
         //assign newQuantity of localStorage product
-        sameProduct.quantityValue = event.value;
+        sameProduct.quantityValue = event.value
 
         //return data in localStorage
+        localStorage.setItem("item", JSON.stringify(registeredItem))
+    }
+}
+//____Delete Product
+function deleteProduct(event, item) {
+    let sameProduct = compareProduct(item);    
+    //if same item in localStorage > delete Product
+    if (sameProduct) {
+        //update price & quantity
+        let array = {value: 0};
+        changeQuantity(array, item);
+        
+        //select HTML article
+        const selectArticle = event.closest("article")
+        //delete HTML article
+        selectArticle.remove()
+        
+        //find item index
+        let index = registeredItem.findIndex(rank => rank === sameProduct);
+        //remove item of localStorage
+        registeredItem.splice(index, 1);
         localStorage.setItem("item", JSON.stringify(registeredItem));
     }
 }
 
 //_____FORM
 //select input Tag
-let formInput = document.getElementsByTagName("input");
+let formInput = document.getElementsByTagName("input")
 
 //select each input
-const firstName = formInput.firstName;
-const lastName = formInput.lastName;
-const address = formInput.address;
-const city = formInput.city;
-const email = formInput.email;
+const firstName = formInput.firstName
+const lastName = formInput.lastName
+const address = formInput.address
+const city = formInput.city
+const email = formInput.email
 
 //creation RegEx
-let firstNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
-let lastNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g;
-let addressRegEx =/^([0-9\s]{0,4})+([A-Za-zÀ-Üà-ü-.\s]{2,60})$/g;
-let cityRegEx =/^([A-Za-zÀ-Üà-ü-.'\s]{2,40})$/g;
-let emailRegEx = /^([\w!#$%&'*+/=?^`{|}~-]{2}(?:[.]?[\w!#$%&'*+/=?^`{|}~-]){0,60})@([a-z]{2,246}[a-z.-]?)+([.][a-z]{2,3})$/g;
+let firstNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g
+let lastNameRegEx = /^([A-Za-zÀ-Üà-ü-\s]{2,20})$/g
+let addressRegEx = /^([0-9\s]{0,4})+([A-Za-zÀ-Üà-ü-.\s]{2,60})$/g
+let cityRegEx = /^([A-Za-zÀ-Üà-ü-.'\s]{2,40})$/g
+let emailRegEx = /^([\w!#$%&'*+/=?^`{|}~-]{2}(?:[.]?[\w!#$%&'*+/=?^`{|}~-]){0,60})@([a-z]{2,246}[a-z.-]?)+([.][a-z]{2,3})$/g
 //Verify formValues
-function form () { 
-    //True/False Value First Name 
+function form() {
+    //True/False Value First Name
     firstName.addEventListener("change", () => {
         if (firstNameRegEx.test(firstName.value)) {
-        }
-        else {
+        } else {
             document.getElementById("firstNameErrorMsg").innerHTML = `Veuillez renseigner un prénom valide`
         }
     })
-    //True/False Value Last Name 
+    //True/False Value Last Name
     lastName.addEventListener("change", () => {
         if (lastNameRegEx.test(lastName.value)) {
-        }
-        else {
+        } else {
             document.getElementById("lastNameErrorMsg").innerHTML = `Veuillez renseigner un nom valide`
         }
     })
     //True/False Value Address Name
     address.addEventListener("change", () => {
         if (addressRegEx.test(address.value)) {
-        }
-        else {
+        } else {
             document.getElementById("addressErrorMsg").innerHTML = `Veuillez renseigner une adresse valide`
         }
     })
     //True/False Value City Name
     city.addEventListener("change", () => {
         if (cityRegEx.test(city.value)) {
-        }
-        else {
+        } else {
             document.getElementById("cityErrorMsg").innerHTML = `Veuillez renseigner une ville valide`
         }
     })
     //True/False Value Email Name
     email.addEventListener("change", () => {
         if (emailRegEx.test(email.value)) {
-        }
-        else {
+        } else {
             document.getElementById("emailErrorMsg").innerHTML = `Veuillez renseigner une adresse email valide`
         }
     })
-
 }
-form();
+form()
 
 const formValues = {
     firstNameValue: firstName.value,
     lastNameValue: lastName.value,
     addressValue: address.value,
     cityValue: city.value,
-    emailValue: email.value
+    emailValue: email.value,
 }
 
-console.log(formValues);
+console.log(formValues)
 
 const sendOrder = {
-    formValues, registeredItem
-};
+    formValues,
+    registeredItem,
+}
 
 // const p = () => {
 //     fetch (`http://localhost:3000/api/products/order`, {
@@ -262,8 +294,3 @@ const sendOrder = {
 //         }
 //     })
 // }
-
-
-
-    
-    
